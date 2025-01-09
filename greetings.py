59df980c1af411a6890c63c02449601ae276849f
IkimazeI <kimaze1122@gmail.com>
2025-01-09 16:54:57 +0300

    Add files via upload
    Задание 1

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = 'Alexey'
+title = 'MyWork'
+content = 'Мне нужно выполнить задание'
+status = 'Ожидание'
+created_date = '30-12-2024'
+issue_date = '09-01-2025'
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Содержимое заметки:', content)
+print('Статус заметки (выполнено, ожидание, не выполнено):', status)
+print('Дата создания заметки:', created_date)
+print('Дата истечения срока заметки:', issue_date)
