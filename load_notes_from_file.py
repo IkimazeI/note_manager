e35f801555a5c1a7d06510d2aed38abcbc539457
ikimazei <lopinovr@mail.ru>
2025-01-30 18:52:30 +0300

    Задание 17

--- load_notes_from_file.py
+++ load_notes_from_file.py
@@ -1,4 +1,6 @@
 import os
+from logging import exception
+
 
 def create_default_note_file(filename):
     # заглушка на случай, если файла нет или он пустой
@@ -14,39 +16,44 @@ def create_default_note_file(filename):
 
 def read_notes_from_file(filename):
     # проверка файла на существование и заполненность
-    if not os.path.exists(filename) or os.path.getsize(filename) == 0:
-        create_default_note_file(filename)
+    try:
+        if not os.path.exists(filename) or os.path.getsize(filename) == 0:
+            create_default_note_file(filename)
 
-    notes = []
-    note = {}
+        notes = []
+        note = {}
 
-    with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            # сносим пробелы и перевод строки
-            line = line.strip()
-            # скипаем пустые строки
-            if not line:
-                continue
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                # сносим пробелы и перевод строки
+                line = line.strip()
+                # скипаем пустые строки
+                if not line:
+                    continue
 
-            # прямое присвоение в зависимости от порядка строк
-            if line.startswith("Имя пользователя:"):
-                note['username'] = line.split(': ', 1)[1]
-            elif line.startswith("Заголовок:"):
-                note['title'] = line.split(': ', 1)[1]
-            elif line.startswith("Описание:"):
-                note['content'] = line.split(': ', 1)[1]
-            elif line.startswith("Статус:"):
-                note['status'] = line.split(': ', 1)[1]
-            elif line.startswith("Дата создания:"):
-                note['created_date'] = line.split(': ', 1)[1]
-            elif line.startswith("Дедлайн:"):
-                note['issue_date'] = line.split(': ', 1)[1]
+                # прямое присвоение в зависимости от порядка строк
+                if line.startswith("Имя пользователя:"):
+                    note['username'] = line.split(': ', 1)[1]
+                elif line.startswith("Заголовок:"):
+                    note['title'] = line.split(': ', 1)[1]
+                elif line.startswith("Описание:"):
+                    note['content'] = line.split(': ', 1)[1]
+                elif line.startswith("Статус:"):
+                    note['status'] = line.split(': ', 1)[1]
+                elif line.startswith("Дата создания:"):
+                    note['created_date'] = line.split(': ', 1)[1]
+                elif line.startswith("Дедлайн:"):
+                    note['issue_date'] = line.split(': ', 1)[1]
 
-        # добавляем заметку в список, если она не пустая
-        if note:
-            notes.append(note)
+            # добавляем заметку в список, если она не пустая
+            if note:
+                notes.append(note)
 
-    return notes
+        return notes
+    except FileNotFoundError:
+        print('Файл filename не найден. Создан новый файл.')
+    except:
+        print('Ошибка при чтении файла filename. Проверьте его содержимое.')
 
 filename = 'filename.txt'
 result = read_notes_from_file(filename)
