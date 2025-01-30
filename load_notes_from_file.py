82e94b2e6cd9465a25cadbc7116116edf1f3009b
IkimazeI <kimaze1122@gmail.com>
2025-01-30 17:08:09 +0300

    Задание 17

---
+++ load_notes_from_file.py
@@ -0,0 +1,55 @@
+import os
+
+def create_default_note_file(filename):
+    # заглушка на случай, если файла нет или он пустой
+    default_content = """Имя пользователя: Алексей
+Заголовок: Список покупок
+Описание: Купить продукты
+Статус: новая
+Дата создания: 27-11-2024
+Дедлайн: 30-11-2024
+"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(default_content.strip())
+
+def read_notes_from_file(filename):
+    # проверка файла на существование и заполненность
+    if not os.path.exists(filename) or os.path.getsize(filename) == 0:
+        create_default_note_file(filename)
+
+    notes = []
+    note = {}
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            # сносим пробелы и перевод строки
+            line = line.strip()
+            # скипаем пустые строки
+            if not line:
+                continue
+
+            # прямое присвоение в зависимости от порядка строк
+            if line.startswith("Имя пользователя:"):
+                note['username'] = line.split(': ', 1)[1]
+            elif line.startswith("Заголовок:"):
+                note['title'] = line.split(': ', 1)[1]
+            elif line.startswith("Описание:"):
+                note['content'] = line.split(': ', 1)[1]
+            elif line.startswith("Статус:"):
+                note['status'] = line.split(': ', 1)[1]
+            elif line.startswith("Дата создания:"):
+                note['created_date'] = line.split(': ', 1)[1]
+            elif line.startswith("Дедлайн:"):
+                note['issue_date'] = line.split(': ', 1)[1]
+
+        # добавляем заметку в список, если она не пустая
+        if note:
+            notes.append(note)
+
+    return notes
+
+
+# Пример использования
+filename = 'filename.txt'
+result = read_notes_from_file(filename)
+print(result)
